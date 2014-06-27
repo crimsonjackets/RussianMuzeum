002b5b463903b78f7a5e8a8fc9e7ef3f5f07631d
Richard Topchiy <richard@richards-mbp.dlink>
2014-06-27 04:57:45 +0400

    Beginning CoreData implementation

--- rusmuz2/AppDelegate.h
+++ rusmuz2/AppDelegate.h
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.h
-//  rusmuz
+//  rusmuz2
 //
-//  Created by Richard Topchiy on 13/05/14.
+//  Created by Richard Topchiy on 27/06/14.
 //  Copyright (c) 2014 Richard Topchiy. All rights reserved.
 //
 
@@ -11,5 +11,12 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 @end
---
+++ rusmuz2/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  rusmuz2
+//
+//  Created by Richard Topchiy on 27/06/14.
+//  Copyright (c) 2014 Richard Topchiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
